ca5e0d4f78ba26884cc205adf5c11ba4abc880e9
Pau Ballart Godoy <pballart@gmail.com>
2014-10-31 15:33:35 +0100

    prefix deleted, data source improved. NOT WORKING

---
+++ ARApp/Classes/Configuration.h
@@ -0,0 +1,13 @@
+//
+//  Configuration.h
+//  ARApp
+//
+//  Created by Pau Ballart Godoy on 31/10/14.
+//  Copyright (c) 2014 SolArt Apps. All rights reserved.
+//
+
+#ifndef ARApp_Configuration_h
+#define ARApp_Configuration_h
+
+
+#endif
