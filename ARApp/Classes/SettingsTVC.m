ee294b64ee8880b3e3c3dea46c6cc45fba0ac86a
Pau Ballart Godoy <pballart@gmail.com>
2014-12-08 12:54:28 +0100

    Settings screen done

--- ARApp/Classes/SettingsTVC.m
+++ ARApp/Classes/SettingsTVC.m
@@ -9,12 +9,19 @@
 #import "SettingsTVC.h"
 #import <MessageUI/MessageUI.h>
 #import "AppDelegate.h"
+#import "WebVC.h"
 
-#define facebook @"fb://requests"
-#define appleStore @"http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=284417350&mt=8&uo=6"
-#define webpage @"http://147.83.39.196/gloop/admin/templates/frontend/"
+#define FACEBOOK_GLOOP_PAGE @"http://www.facebook.com/takeagloop"
+#define FACEBOOK_GLOOP_PAGE_URI @"fb://profile/992206174129331"
+#define INSTAGRAM_GLOOP_PAGE @"http://www.instagram.com/takeagloop"
+#define INSTAGRAM_GLOOP_PAGE_URI @"instagram://user?username=takeagloop"
+#define APP_STORE_LINK @"https://itunes.apple.com/us/app/gloop!/id949189876"
+#define GLOOP_WEBPAGE @"http://www.takeagloop.com"
+#define FAQ_WEBPAGE @"http://www.takeagloop.com/faq"
+#define TOS_WEBPAGE @"http://www.takeagloop.com/tos"
+#define ABOUT_WEBPAGE @"http://www.takeagloop.com/about"
 
-@interface SettingsTVC () <MFMailComposeViewControllerDelegate>
+@interface SettingsTVC () <MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>
 
 
 @end
@@ -24,11 +31,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    UIImageView *tempImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"settings_background"]];
+    [tempImageView setFrame:self.tableView.frame];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.tableView.backgroundView = tempImageView;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -44,6 +50,34 @@
 }
 
 
+- (UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.bounds.size.width, 30)];
+    [headerView setBackgroundColor:[UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:0.3]];
+    
+    
+    UILabel *label = [[UILabel alloc] initWithFrame: CGRectMake(25,0, 300, 50)];
+    // label.backgroundColor = [UIColor yellowColor];
+    label.autoresizingMask = UIViewAutoresizingFlexibleRightMargin;
+    label.textColor = [UIColor whiteColor];
+    [headerView addSubview:label];
+    if (section == 0){
+        label.text=@"SUPPORT";
+    }else if(section ==1 ){
+        label.text=@"SOCIAL";
+    }else if(section ==2 ){
+        label.text=@"ABOUT GLOOP!";
+    }else if(section ==3 ){
+        label.text=@"LOGOUT";
+    }
+    return headerView;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 
     UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
@@ -57,40 +91,119 @@
 
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
-    if ([selectedCell.reuseIdentifier isEqualToString:@"Invita"]){
-        NSLog(@"Invita");
-        [self abrirMail];
-    }
-    else if ([selectedCell.reuseIdentifier isEqualToString:@"Enviar"]){
-        NSLog(@"Enviar");
-        [self abrirMail];
-    }
-    else if ([selectedCell.reuseIdentifier isEqualToString:@"Reseña"]){
-        NSURL *url = [NSURL URLWithString:appleStore];
-        [[UIApplication sharedApplication] openURL:url];
-        NSLog(@"Reseña");
-    }
-    else if ([selectedCell.reuseIdentifier isEqualToString:@"Legal"]){
-        NSLog(@"Legal");
-    }
-    else if ([selectedCell.reuseIdentifier isEqualToString:@"Web"]){
-        NSURL *url = [NSURL URLWithString:webpage];
-        [[UIApplication sharedApplication] openURL:url];
-        NSLog(@"Web");
-    }
-    else if ([selectedCell.reuseIdentifier isEqualToString:@"Facebook"]){
-        NSURL *url = [NSURL URLWithString:facebook];
-        [[UIApplication sharedApplication] openURL:url];
-        NSLog(@"Facebook");
-    }
-    else if ([selectedCell.reuseIdentifier isEqualToString:@"Logout"]){
-        [self logout];
-        NSLog(@"Logout");
+    switch (indexPath.section) {
+        case 0:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                    //FAQ
+                    WebVC *webViewVC = [[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateViewControllerWithIdentifier:@"WebVC"];
+                    webViewVC.url = FAQ_WEBPAGE;
+                    [self presentViewController:webViewVC animated:YES completion:nil];
+                }
+                    break;
+                case 1:
+                    //Send feedback
+                    [self sendFeedbackMail];
+                    break;
+                case 2:
+                    //Make a review
+                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:APP_STORE_LINK]];
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+            break;
+        case 1:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                    //Invite friends
+                    if ([MFMessageComposeViewController canSendText]) {
+                        MFMessageComposeViewController *smsVC = [[MFMessageComposeViewController alloc] init];
+                        smsVC.messageComposeDelegate = self;
+                        smsVC.body = [@"Hey! I know you like beers so you will like this new beer app 😜 Check it out! " stringByAppendingString:APP_STORE_LINK];
+                        [self presentViewController:smsVC animated:YES completion:nil];
+                    }
+                }
+                    break;
+                case 1:
+                    //facebook
+                    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:FACEBOOK_GLOOP_PAGE_URI]]) {
+                        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:FACEBOOK_GLOOP_PAGE_URI]];
+                    } else {
+                        WebVC *webViewVC = [[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateViewControllerWithIdentifier:@"WebVC"];
+                        webViewVC.url = FACEBOOK_GLOOP_PAGE;
+                        [self presentViewController:webViewVC animated:YES completion:nil];
+                    }
+                    break;
+                case 2:
+                    //instagram
+                    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:INSTAGRAM_GLOOP_PAGE_URI]]) {
+                        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:INSTAGRAM_GLOOP_PAGE_URI]];
+                    } else {
+                        WebVC *webViewVC = [[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateViewControllerWithIdentifier:@"WebVC"];
+                        webViewVC.url = INSTAGRAM_GLOOP_PAGE;
+                        [self presentViewController:webViewVC animated:YES completion:nil];
+                    }
+                    break;
+                case 3:
+                {
+                    //website
+                    WebVC *webViewVC = [[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateViewControllerWithIdentifier:@"WebVC"];
+                    webViewVC.url = GLOOP_WEBPAGE;
+                    [self presentViewController:webViewVC animated:YES completion:nil];
+                }
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+            break;
+        case 2:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                    //terms of user
+                    WebVC *webViewVC = [[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateViewControllerWithIdentifier:@"WebVC"];
+                    webViewVC.url = TOS_WEBPAGE;
+                    [self presentViewController:webViewVC animated:YES completion:nil];
+                }
+                    break;
+                case 1:
+                {
+                    //about plusdimension
+                    WebVC *webViewVC = [[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateViewControllerWithIdentifier:@"WebVC"];
+                    webViewVC.url = ABOUT_WEBPAGE;
+                    [self presentViewController:webViewVC animated:YES completion:nil];
+
+                }
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+            break;
+        case 3:
+        {
+            //Logout
+            [self logout];
+        }
+            break;
+            
+        default:
+            break;
     }
 }
 
-- (IBAction)abrirMail {
+- (IBAction)sendFeedbackMail {
     
     // Comprobamos si nuestro dispositivo puede enviar mails en este momento
     if ([MFMailComposeViewController canSendMail])
@@ -98,22 +211,22 @@
         MFMailComposeViewController *mailer = [[MFMailComposeViewController alloc] init];
         mailer.mailComposeDelegate = self;
         
-        [mailer setSubject:@"Asunto del mensaje"];
+        [mailer setSubject:@"Feedback from Gloop! app"];
         
-        NSArray *toRecipients = [NSArray arrayWithObjects:@"usuario1@gmail.com", nil];
+        NSArray *toRecipients = [NSArray arrayWithObjects:@"support@takeagloop.com", nil];
         
         [mailer setToRecipients:toRecipients];
         
-        NSString *emailBody = @"Texto que incluirá el <b>email</b> que vamos a enviar.";
+        NSString *emailBody = @"I've been using the Gloop! app and I wanted you to know that...";
         
-        [mailer setMessageBody:emailBody isHTML:YES];
+        [mailer setMessageBody:emailBody isHTML:NO];
         
         [self presentViewController:mailer animated:YES completion:nil];
     }
     else
     {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                        message:@"Tu dispositivo no permite enviar correos desde esta app."
+                                                        message:@"You cannot send e-mails right now."
                                                        delegate:nil
                                               cancelButtonTitle:@"Ok"
                                               otherButtonTitles: nil];
@@ -122,27 +235,13 @@
     
 }
 
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    [self.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
 - (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error
 {
-    switch (result)
-    {
-        case MFMailComposeResultCancelled:
-            NSLog(@"El usuario ha cancelado el envio del email.");
-            break;
-        case MFMailComposeResultSaved:
-            NSLog(@"El usuario ha guardado el mensaje.");
-            break;
-        case MFMailComposeResultSent:
-            NSLog(@"El usuario ha enviado el mensaje y ha quedado pendiente.");
-            break;
-        case MFMailComposeResultFailed:
-            NSLog(@"El mensaje no se ha guardado ni enviado por algún error.");
-            break;
-        default:
-            NSLog(@"El mensaje no se ha enviado.");
-            break;
-    }
-    // Eliminamos el controlador que muestra el envio del correo
     [self.presentedViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
